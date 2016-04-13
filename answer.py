da4824d13703a094eab73daedab13ed6fb2b9b3c
wangyb <wangybi@aliyu.com>
2016-04-13 17:46:10 +0800

    local_file_crawler

---
+++ answer.py
@@ -0,0 +1,32 @@
+from bs4 import BeautifulSoup
+
+
+def get_page(path):
+    try:
+        web_data = open(path, 'r')
+        soup = BeautifulSoup(web_data, 'lxml')  # instance of BeautifulSoup
+        images = soup.select("body > div > div > div.col-md-9 > div > div > div > img ")
+        titles = soup.select("body > div > div > div.col-md-9 > div > div > div > div.caption > h4 > a")
+        prices = soup.select("body > div > div > div.col-md-9 > div > div > div > div.caption > h4.pull-right")
+        reviews = soup.select("body > div > div > div.col-md-9 > div > div > div > div.ratings > p.pull-right")
+        stars = soup.select("body > div > div > div.col-md-9 > div > div > div > div.ratings > p:nth-of-type(2)")
+        for title, image, review, price, star in zip(titles, images, reviews, prices, stars):
+            data = {
+                "image": image.get("src"),
+                "title": title.get_text(),
+                "price": price.get_text(),
+                "review": review.get_text(),
+                "star": ['*' * len(star.find_all('span',  class_='glyphicon glyphicon-star'))]
+            }
+            print(data)
+    except IOError:
+        print("Error: can\'t find file or read data")
+    finally:
+        # 检查文件中是否存在某个变量：locals()
+        if 'web_data' in locals():
+            web_data.close()
+            print("file closed")
+
+if __name__ == "__main__":
+    local_path = './resources/index.html'
+    get_page(local_path)
